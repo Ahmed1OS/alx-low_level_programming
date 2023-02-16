eb66cb7d9f2b0d691a6d8c3a45a2e3440d7d1b44
Ahmed1OS <ahmedo1github@gmail.com>
2023-02-16 07:48:14 -0800

    fff

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
+
 /**
  * main - prinfts "  with proper grammar, but the outcome is a piece of art " followed by new line
  * Return: 0 (Sucess)
  */
+
 int main(void)
+
 
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
