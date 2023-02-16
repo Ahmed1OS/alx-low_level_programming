7bc04768ba7b3dc2883429d549aae7f9b6d885c7
Ahmed1OS <ahmedo1github@gmail.com>
2023-02-16 07:56:43 -0800

    shit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 
 /**
  * main - prinfts "  with proper grammar, but the outcome is a piece of art " followed by new line
- * Return: 0 (Sucess)
+ * Return: always 0.
  */
 
 int main(void)
 
-
+Vv
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
